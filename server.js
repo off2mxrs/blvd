334e163376afb14dd611bf9df5eb3233850bc4a7
off2mxrs <off2mxrs@gmail.com>
2021-08-11 11:10:22 -0700

    Rowdy Logger set

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // REQUIRED MODULES ////////////
 const express = require('express')
+const rowdy = require('rowdy-logger')
 
 // VARIABLES //////////////
 const app = express()
 const port = 4000
+const rowdyResults = rowdy.begin(app)
 
 ///////// INDEX ROUTE //////////////
 app.get('/', (req, res) => {
@@ -13,5 +15,5 @@ app.get('/', (req, res) => {
 /// START SERVER ////////////
 app.listen(port, () => {
     console.log(` 🎨 💿 👨‍🎤 The server is running on port: ${port} 🎨 💿 👨‍🎤 `);
-    //rowdyResults.print()
+    rowdyResults.print()
 })
