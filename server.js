7c764f048727516a01e41a5d673260e02180a8c0
off2mxrs <off2mxrs@gmail.com>
2021-08-11 11:16:03 -0700

    New Route set

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const app = express()
 const port = 4000
 const rowdyResults = rowdy.begin(app)
 
-///////// INDEX ROUTE //////////////
-app.get('/', (req, res) => {
+///////// 🏁 INDEX ROUTE //////////////
+app.get('/blvd', (req, res) => {
     res.send('WELCOME TO BLVD')
 })
 
+///////// 🆕 NEW ROUTE //////////////
+app.get('/blvd/new', (req, res) => {
+    res.send('NEW BLVD Profile page')
+})
 /// START SERVER ////////////
 app.listen(port, () => {
     console.log(` 🎨 💿 👨‍🎤 The server is running on port: ${port} 🎨 💿 👨‍🎤 `);
