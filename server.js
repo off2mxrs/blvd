a1ce5b962538665fad897157ac7caa63a39b4c60
off2mxrs <off2mxrs@gmail.com>
2021-08-11 10:59:19 -0700

    Server running

---
+++ server.js
@@ -0,0 +1,12 @@
+// REQUIRED MODULES ////////////
+const express = require('express')
+
+// VARIABLES //////////////
+const app = express()
+const port = 4000
+
+/// START SERVER ////////////
+app.listen(port, () => {
+    console.log(` 🎨 💿 👨‍🎤 The server is running on port: ${port} 🎨 💿 👨‍🎤 `);
+    //rowdyResults.print()
+})
