23499ae290eb6e19df4ddd6d3343e104d3424bd9
off2mxrs <off2mxrs@gmail.com>
2021-08-12 16:12:49 -0700

    Profile Image Url Upload added to new.ejs form

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     category: {type: String, required: true},
     location: {type: String, required: true},
+    profileImage: String,
 })
 
 
